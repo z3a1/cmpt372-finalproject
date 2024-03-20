35ebfc63a9840d7c26e43b4fe4039cba4cb009db
Andre Luong <97815138+andreluong@users.noreply.github.com>
2024-03-19 22:23:40 -0700

    Add google maps

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,15 @@ const cors = require("cors")
 require('dotenv').config()
 const axios = require("axios")
 const app = express()
+
+// Parsing body
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.use(cors())
+
+// Google maps
+const googleMaps = require('./GoogleMaps/googleMaps')
+app.use('/maps', googleMaps)
 
 
 app.listen(process.env.PORT, () => {
